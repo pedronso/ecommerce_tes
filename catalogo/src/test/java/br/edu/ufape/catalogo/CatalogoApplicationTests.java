6a92626696cadedd63f51befec1629bafd6c2e2a
Pedro Caetano Araújo Alves <pedro.alves@greenpaperless.com.br>
2024-07-25 08:30:57 -0300

    first commit

---
+++ catalogo/src/test/java/br/edu/ufape/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ preco/src/test/java/br/edu/ufape/preco/PrecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.preco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
